6bbe0781e4eef1d0dcfd54c4c83728074a31b3ef
remy taglang <remy.taglang@gmail.com>
2017-09-17 19:30:08 +0200

    Simple form for creating restaurant and critique

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
-  belongs_to :store
+  belongs_to :store, optional: true
+  validates :category, presence: :true, inclusion: { in: ["restaurant", "bar"], message: "Ce n'est pas bon" }
 end
--- app/models/critique.rb
+++ app/models/critique.rb
@@ -1,3 +1,3 @@
 class Critique < ApplicationRecord
-  belongs_to :store
+  belongs_to :store, optional: true
 end
